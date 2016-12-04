feac09ebab801bfd3795591e667f9d15b59205c6
ntager18 <ntager18@amherst.edu>
2016-12-04 15:30:59 -0500

    Update mux4ALU.sv

--- mux4ALU.sv
+++ mux4ALU.sv
@@ -1,8 +1,13 @@
 	module mux4(input logic [3:0] d0, d1, d2, d3,
 		    input logic [1:0],
 		    output logic [3:0] y);
+		
 		logic [3:0] low, high;
 		
+		parameter A=6'b1000000;//add 
+   		parameter F=6'b100110;//nor
+  		parameter H=6'b000100;//not
+
 		mux2 lowmux (d0, d1, s[0], low);
 		mux2 highmux (d2, d3, s[0], high);
 		mux2 finalmux(low, high, s[1], y);
