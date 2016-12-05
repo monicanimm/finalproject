b62fcdb00e1b235e8002e671aa91be28e624f2b9
ntager18 <ntager18@amherst.edu>
2016-12-04 20:09:52 -0500

    Update mux8to1B8.sv

--- mux8to1B8.sv
+++ mux8to1B8.sv
@@ -5,8 +5,8 @@ module mux8to1B8 (input logic C2, input logic C1, input logic C0, input logic [7
 
 	integer i;
 	
-	mux4to1B4 m0(C1,C0,I3[3:0],I2[3:0],I1[3:0],I0[3:0],O[3:0]);
-	mux4to1B4 m1(C1,C0,I3[7:4],I2[7:4],I1[7:4],I0[7:4],O[7:4]);
+	mux8to1B4 m0(C2,C1,C0,I7[3:0],I6[3:0],I5[3:0],I4[3:0],I3[3:0],I2[3:0],I1[3:0],I0[3:0],O[3:0]);
+	mux8to1B4 m1(C2,C1,C0,I7[7:4],I6[7:4],I5[7:4],I4[7:4],I3[7:4],I2[7:4],I1[7:4],I0[7:4],O[7:4]);
 	
 
 
