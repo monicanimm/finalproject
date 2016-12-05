d913d4a2366a77e8ef9b1e1a69a14374adc05166
monicanimm <monicanimm@gmail.com>
2016-12-04 19:53:52 -0500

    Update mux8to1B1.sv

--- mux8to1B1.sv
+++ mux8to1B1.sv
@@ -4,8 +4,8 @@ module mux8to1B1(C2,C1,C0,I7,I6,I5,I4,I3,I2,I1,I0,O1);
 input logic C2, C1, C0, I7, I6, I5, I4, I2, I1, I3, I0;
 output logic O1;
 
-	assign O1 = (C2 & C1 & C0 & I3) |
-	(C1 & ~C0 & I2) |
+	assign O1 = (C1 & C0 & I3) |
+		(C1 & ~C0 & I2) |
 	(~C1 & C0 & I1) |
 	(~C1 & ~C0 & I0) | ;
 	
