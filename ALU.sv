c6fea986d9a16eec13ac146b89d5e08d8090581c
monicanimm <monicanimm@gmail.com>
2016-12-06 17:44:09 -0500

    Update ALU.sv

--- ALU.sv
+++ ALU.sv
@@ -12,20 +12,6 @@ module ALU(input logic  [31:0] I1,
 	
    logic [31:0] result;
 
-   //parameter A=6'b1000000;//add 
-   //parameter F=6'b100110;//nor
-   //parameter H=6'b000100;//not
-		//rolv
-	//rorv
-	//bleu
-
- 
-   /*case(Selector)
-     A: result=I1+I2;
-     F: result=I1 ~| I2;
-     H: result=~I1;
-   endcase; // case (Selector)*/
-   //need to set up a multiplexer
 	assign add = I1 + I2;
 	assign nor = ~ (I1 | I2);
 	assign not = ~(I1);
@@ -38,7 +24,6 @@ module ALU(input logic  [31:0] I1,
 	assign selector3=C2;
 	
 	mux8to1B32 mux0(C2,C1,C0,add,nor,not,rolv,rorv,bleu,32'b0,32'b0,result);
-   //assign sum = I1 + I2;
    
    assign ALUResult = result;
 	
