3588deb2d31b8999b9aacde332c4e69bdbd55c66
monicanimm <monicanimm@gmail.com>
2016-12-06 17:43:06 -0500

    Update ALU.sv

--- ALU.sv
+++ ALU.sv
@@ -5,8 +5,9 @@ As you are given it, this ALU only implements Add
 module ALU(input logic  [31:0] I1,
 
 	   input logic [31:0] I2,
-	   input logic [5:0] Selector,//5 bits
-	   output logic [31:0] O
+	   input logic [0] selector1, input logic[0] selector2, input logic[0] selector3,
+					    //5 bits
+	   output logic [31:0] ALUresult
 	   );
 	
    logic [31:0] result;
@@ -31,10 +32,14 @@ module ALU(input logic  [31:0] I1,
 	assign rolv = 0;
 	assign rorv = 0;
 	assign bleu = 0;
-	   
-    
+	
+	assign selector1=C0;
+	assign selector2=C1;
+	assign selector3=C2;
+	
+	mux8to1B32 mux0(C2,C1,C0,add,nor,not,rolv,rorv,bleu,32'b0,32'b0,ALUResult);
    //assign sum = I1 + I2;
    
-   assign O = result;
+   assign ALUResult = result;
 	
 endmodule
